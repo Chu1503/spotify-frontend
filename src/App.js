07e844d245a1801c9ac4ca1b92b293cf6578e796
Chu1503 <shivakash2003@gmail.com>
2024-11-05 21:12:12 +0530

    token

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-// src/App.js
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 // Import Components and Pages
 import Sidebar from "./components/Sidebar";
@@ -23,14 +27,16 @@ function App() {
 
   useEffect(() => {
     const hash = window.location.hash;
-    let token = window.localStorage.getItem("token");
+    let token = window.localStorage.getItem("access_token");
 
     if (!token && hash) {
       const params = new URLSearchParams(hash.substring(1));
       token = params.get("access_token");
+      const refreshToken = params.get("refresh_token");
 
-      window.location.hash = "";
-      window.localStorage.setItem("token", token);
+      window.location.hash = ""; // Clear the hash
+      window.localStorage.setItem("access_token", token);
+      window.localStorage.setItem("refresh_token", refreshToken);
     }
 
     setToken(token);
@@ -49,6 +55,7 @@ function App() {
         })
         .catch((error) => {
           console.error("Error fetching user data:", error);
+          handleLogout(); // Logout if token is invalid
         });
 
       axios
@@ -70,7 +77,8 @@ function App() {
     setToken("");
     setUser(null);
     setPlaylistsCount(0);
-    window.localStorage.removeItem("token");
+    window.localStorage.removeItem("access_token");
+    window.localStorage.removeItem("refresh_token");
   };
 
   if (!token) {
@@ -98,22 +106,10 @@ function App() {
               path="/profile"
               element={<Profile user={user} playlistsCount={playlistsCount} />}
             />
-            <Route
-              path="/top-artists"
-              element={<TopArtists token={token} />}
-            />
-            <Route
-              path="/top-tracks"
-              element={<TopTracks token={token} />}
-            />
-            <Route
-              path="/recent"
-              element={<Recent token={token} />}
-            />
-            <Route
-              path="/playlists"
-              element={<Playlists token={token} />}
-            />
+            <Route path="/top-artists" element={<TopArtists token={token} />} />
+            <Route path="/top-tracks" element={<TopTracks token={token} />} />
+            <Route path="/recent" element={<Recent token={token} />} />
+            <Route path="/playlists" element={<Playlists token={token} />} />
             <Route
               path="/recommendations"
               element={<Recommendations token={token} />}
@@ -130,10 +126,7 @@ function App() {
               path="/artist/:id"
               element={<ArtistDetail token={token} />}
             />
-            <Route
-              path="/playrec/:id" // Recommendations page route
-              element={<PlayRec token={token} />}
-            />
+            <Route path="/playrec/:id" element={<PlayRec token={token} />} />
             <Route path="*" element={<Navigate to="/profile" replace />} />
           </Routes>
         </main>
